65e2e59a2ce42a53c7ce0bbdd1c60dc1ff4a8be8
Khang Nguyen <m.khang8123@gmail.com>
2025-09-20 14:07:43 +0700

    chore: set up Docker

--- config/rabbitmq.js
+++ config/rabbitmq.js
@@ -7,36 +7,34 @@ let shuttingDown = false
  * Connect to RabbitMQ and create a channel (singleton).
  */
 export async function getChannel() {
-  if (channel) return channel
-
-  try {
-    connection = await amqp.connect(
-      process.env.RABBITMQ_URL || 'amqp://localhost'
-    )
-
-    connection.on('error', (err) => {
-      console.error('[RabbitMQ] Connection error: ', err.message)
-    })
-
-    connection.on('close', () => {
-      console.warn('[RabbitMQ] Connection closed. Reconnecting...')
-      channel = null
-      // Only reconnect on unexpected disconnects
-      if (!shuttingDown) {
-        setTimeout(getChannel, 10000) // reconnect after 10s
-      } else {
-        shuttingDown = false
-      }
-    })
-
-    channel = await connection.createChannel()
-    console.log('[RabbitMQ] ✅ Connected and Channel created')
-
-    return channel
-  } catch (error) {
-    console.error('[RabbitMQ] Failed to connect:', error.message)
-    setTimeout(getChannel, 10000)
+  while (!channel) {
+    try {
+      connection = await amqp.connect(
+        process.env.RABBITMQ_URL || 'amqp://localhost'
+      )
+      connection.on('error', (err) => {
+        console.error('[RabbitMQ] Connection error: ', err.message)
+      })
+      connection.on('close', () => {
+        console.warn('[RabbitMQ] Connection closed. Reconnecting...')
+        channel = null
+        // Only reconnect on unexpected disconnects
+        if (!shuttingDown) {
+          setTimeout(getChannel, 10000) // reconnect after 10s
+        } else {
+          shuttingDown = false
+        }
+      })
+      channel = await connection.createChannel()
+      console.log('[RabbitMQ] ✅ Connected and Channel created')
+      return channel
+    } catch (error) {
+      console.error('[RabbitMQ] Failed to connect:', error.message)
+      console.log('[RabbitMQ] Reconnect in 10s...')
+      await new Promise((resolve) => setTimeout(resolve, 10000))
+    }
   }
+  return channel
 }
 
 /**
